04fa40cf2ad54281a9b8bc1dc01161eb10be25eb
Mariona Roca <marionaroca@MacBook-MacBook-Pro-de-Mariona.local>
2024-10-27 22:55:11 +0100

    funciona

---
+++ src/afegirALaLlista.js
@@ -0,0 +1,23 @@
+// const url = 'http://localhost:3000/llista';
+
+document.querySelector(".form button").addEventListener("click", (e)=>{
+
+    let nouElement = document.querySelector('#newAutocuidado').value;
+    console.log("nouElement ", nouElement);
+
+    if (nouElement == '') return;
+
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({item: nouElement}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    .then(response => response.json())
+    .then(({data}) => {
+        console.log("nou element enviat correctament: ", nouElement);
+        document.querySelector('#newAutocuidado').value=null;
+    })
+    .catch(err => console.log(err));
+});
--- src/triarDeLaLlista.js
+++ src/triarDeLaLlista.js
@@ -1,6 +1,20 @@
+const url = 'http://localhost:3000/llista';
+
 document.querySelector(".apreta").addEventListener("click", () => {
-
-    let randomElem = Math.floor(Math.random() * llista.length);
-
-    document.querySelector("#resultat").innerHTML = llista[randomElem];
+    fetch(url,{
+        method: "GET", // or 'PUT'
+        //body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    .then(response => response.json())
+    .then(({data}) => {
+        console.log("data: ", data)
+        let randomElem = Math.floor(Math.random() * data.length);
+        console.log("randomElem: ", randomElem)
+        console.log("data.length: ", data.length)
+        document.querySelector("#resultat").innerHTML = llista[randomElem];
+    })
+    .catch(err => console.log(err));
 })
